4eb01af75abab86a89f0ab6705c28797e13fad03
Arthur Barata <arthurpbarata@gmail.com>
2026-01-29 09:59:47 -0300

    apply real ip

--- scripts/run-loader.sh
+++ scripts/run-loader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f test-loader.yaml
+kubectl apply -f config/test-loader.yaml
 
 echo "Test started. Waiting for loader pod to complete..."
 sleep 10
@@ -18,4 +18,4 @@ echo "Increasing 30 users up to 180 users"
 sleep 10
 echo "Test completed. Copying results from loader pod..."
 # 1-> number of remote copy 2-> test: microservice or SDS
-source ./copy-result.sh $1 $2
+source scripts/copy-result.sh $1 $2
